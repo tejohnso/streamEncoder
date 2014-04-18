6dc875109da4c6c7954c4bf71e2cd4a10d28e6c8
tejohnso <mail@tyler-johnson.ca>
2014-04-18 13:17:50 -0400

    Fix incorrect interpretation of transforms.
    The examples were tested more closely and changes were made.
    The transformations aren't compounded onto one another.
    And it's not the keyboard that is shifting, it's a target pointer that
    moves position for every instruction.  And each key position processes
    all instructions before the next key pointer is calculated.
    
    Previously, the keys themselves were shifting positions, and the entire
    keyboard would be transformed before the next transformatoin instruction
    was considered.  This is a much more complicated encoding but is not
    correct as per the examples.

--- transform.js
+++ transform.js
@@ -1,37 +1,29 @@
 "use strict";
-var fs = require('fs');
-var stdKbd = require('./lib/standardKeyboard')();
-var mapKbd;
-var mappingDirectives;
-var mapArr = [];
-var printKeyboardMap = require('./lib/utils').printKeyboardMap;
-var thisFile = require('./lib/utils').thisFile;
-var usageText = "Usage: node " + thisFile() + " transform-file data-file [--showmap]";
-var streamTransformer;
-var dataStream;
+var fs = require('fs')
+   ,mappingDirectives
+   ,mapArr = []
+   ,usageText = require('./lib/utils').usageText
+   ,streamTransformer
+   ,dataStream;
 
 try {
-  mappingDirectives = require('./lib/readDirectives')();
+  mappingDirectives = require('./lib/readDirectives');
 } catch(e) {
   console.error(e + '\n' + usageText);
   return -1;
 }
 
-mapKbd = require('./lib/mappedKeyboard')(mappingDirectives);
-
-if (process.argv[4] && process.argv[4].substr(0,6) === '--show') {
-  printKeyboardMap(stdKbd, mapKbd);
-}
-
-require('./lib/utils').iterateBoard(function(i, j) {
-  mapArr[stdKbd[i][j].charCodeAt(0)] = mapKbd[i][j];
+mappingDirectives = mappingDirectives.split(',').map(function(val) {
+  return val.trim().toUpperCase().replace('\n','');
 });
+mapArr = require('./lib/mappedKeyboard')(mappingDirectives);
 
 streamTransformer = require('./lib/TransformPipe')(mapArr);
 streamTransformer.pipe(process.stdout);
 dataStream = fs.createReadStream(process.argv[3]);
 dataStream.on('error', function(e) {
   console.log("Error " + e.code + " on accessing file "  + e.path + "\n" + usageText);
+  return -1;
 });
 
 dataStream.pipe(streamTransformer);
